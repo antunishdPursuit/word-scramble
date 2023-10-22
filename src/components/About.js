591474ad576a0de3829222d77e83432672c883fd
JenniferEinstein <jennifereinstein@gmail.com>
2023-10-22 13:28:10 -0400

    change background color of About page. Start changing text. Change widths. Change Name styling.

--- src/components/About.js
+++ src/components/About.js
@@ -16,14 +16,14 @@ function About() {
             <div className="about about-Dennys about-dev">
                 <img src={imgDennys} alt="Dennys Antunish headshot" className="about-headshot"/>
                 <div className="about-devinfo">
-                    <h2>Dennys Antunish</h2>
+                    <a className="about-name-url" href="https://github.com/antunishdPursuit">Dennys Antunish</a>
                     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                 </div>
             </div>
             <div className="about about-Jennifer about-dev">
                 <img src={imgJennifer} className="about-headshot" alt="Jennifer Einstein headshot" />
                 <div className="about-devinfo">
-                    <h2>Jennifer Einstein</h2>
+                    <a className="about-name-url" href="https://github.com/JenniferEinstein">Jennifer Einstein</a>
                     <p className="about-devinfo">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
 
                 </div>
@@ -31,7 +31,7 @@ function About() {
             <div className="about about-Kenti about-dev">
                 <img src={imgKenti} className="about-headshot" alt="Ray Kenti headshot" />
                 <div className="about-devinfo">
-                    <h2><a href="https://github.com/pulse99r"> Ray "Kenti" Johnson</a></h2>
+                    <a className="about-name-url" href="https://github.com/pulse99r">Ray "Kenti" Johnson</a>
                     <p className="about-devinfo">Kenti was the lead developer on this project. A graduate of Udacity's NanoDegree program (August 2022) focusing on Front End Web Development, Kenti is also a professional Jazz Giutarist and Music Educator and strive to create web development projects that benefit people in the music industry, especially in the area of music education and educating children.
                     </p>
                 </div>
