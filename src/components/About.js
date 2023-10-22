ce6d43f1989ca3ec1f8c08acc7d24714740c7c72
JenniferEinstein <jennifereinstein@gmail.com>
2023-10-22 10:37:05 -0400

    About page

--- src/components/About.js
+++ src/components/About.js
@@ -13,23 +13,28 @@ function About() {
         </div>
         <h1>About the Developers</h1>
         <div className="about about-developers">
-            <div className="about about-Dennys about-developer">
+            <div className="about about-Dennys about-dev">
                 <img src={imgDennys} alt="Dennys Antunish headshot" className="about-headshot"/>
                 <div className="about-devinfo">
                     <h2>Dennys Antunish</h2>
                     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                 </div>
             </div>
-            <div className="about about-Jennifer about-developer">
-                <h2>Jennifer Einstein</h2>
+            <div className="about about-Jennifer about-dev">
                 <img src={imgJennifer} className="about-headshot" alt="Jennifer Einstein headshot" />
-                <p className="about-devinfo">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                <div className="about-devinfo">
+                    <h2>Jennifer Einstein</h2>
+                    <p className="about-devinfo">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+
+                </div>
             </div>
-            <div className="about about-Kenti about-developer">
-                <h2>Ray Kenti</h2>
+            <div className="about about-Kenti about-dev">
                 <img src={imgKenti} className="about-headshot" alt="Ray Kenti headshot" />
-                <p className="about-devinfo">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                </p>
+                <div className="about-devinfo">
+                    <h2>Ray Kenti</h2>
+                    <p className="about-devinfo">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    </p>
+                </div>
             </div>
 </div>
         
