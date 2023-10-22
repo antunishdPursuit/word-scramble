cb03efb4c4b4ca5a6080a129e45feb736ae03294
JenniferEinstein <jennifereinstein@gmail.com>
2023-10-22 14:27:20 -0400

    Add font changes. Add minimum bios. Add padding. Et al.

--- src/components/About.js
+++ src/components/About.js
@@ -8,24 +8,25 @@ function About() {
   return (
     <div className="about about-container">
         <div className="about about-application">
-            <h1>About This Application</h1>
-                This word unscramble was created as part of Pursuit's Hackathonpalooza 2023.
+            <div className="about-heading">About This Application</div>
+                <p>This word unscramble was created as part of Pursuit's Hackathon Palooza 2023. </p>
         </div>
-        <h1>About the Developers</h1>
+            <div className="about-heading">About the Developers</div>
+            <p>Dennys, Jennifer, and Kenti are Pursuit fellows in the 9.6. The Pursuit Fellowship is a 12-month, Google-funded software engineering fellowship with a 9% acceptance rate. Project-based curriculum provides hands-on experience with JavaScript, HTML, CSS, Node, Express, React/Redux, PostgreSQL, APIs, Git, Data Structures, Algorithms, sprints, Agile, code reviews, and pair programming.</p>
+
         <div className="about about-developers">
             <div className="about about-Dennys about-dev">
                 <img src={imgDennys} alt="Dennys Antunish headshot" className="about-headshot"/>
                 <div className="about-devinfo">
                     <a className="about-name-url" href="https://github.com/antunishdPursuit">Dennys Antunish</a>
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    <p>Entering the computer field in 2017, Dennys is is especially excited about the developing use of Artificial Intelligence.</p>
                 </div>
             </div>
             <div className="about about-Jennifer about-dev">
                 <img src={imgJennifer} className="about-headshot" alt="Jennifer Einstein headshot" />
                 <div className="about-devinfo">
                     <a className="about-name-url" href="https://github.com/JenniferEinstein">Jennifer Einstein</a>
-                    <p className="about-devinfo">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-
+                    <p className="about-devinfo">Jennifer's first class was called "I Speak Basic to my Apple." Since then, after a time away from ComputerWorld, she has returned and is learning the ins and outs of coding from the teachers at Pursuit. </p>
                 </div>
             </div>
             <div className="about about-Kenti about-dev">
